cd0a2a04d985dad40687bc924c9675e6c3b92312
peauli <preciousajorgba@gmail.com>
2022-06-13 21:35:51 +0100

    modified rsp.py

--- rps.py
+++ rps.py
@@ -1,26 +1,39 @@
 import random
 
-your_choice = input("Enter a choice (R for Rock, P for Paper, S for Scissors): ")
 
-options = ["R", "P", "S"]
-CPU = random.choice(options)
+while True:
+    try:
+        your_choice = input("Enter a choice (R for Rock, P for Paper, S for Scissors): ")
 
-print( "player - {} , CPU - {}".format(your_choice,CPU))
+        options = ["R", "P", "S"]
+        CPU = random.choice(options)
 
-if your_choice == CPU:
-    print(f"You both selected {your_choice}. It's a tie, No winner no Losser!")
-elif your_choice == "R":
-    if CPU == "S":
-        print("Rock beats scissors! You win!")
-    else:
-        print("Paper beats rock! You lose.")
-elif your_choice == "P":
-    if CPU == "R":
-        print("Paper beats rock! You win!")
-    else:
-        print("Scissors beats paper! You lose.")
-elif your_choice == "S":
-    if CPU == "P":
-        print("Scissors beats paper! You win!")
-    else:
-        print("Rock beats scissors! You lose.")
+        print( "player ({}), CPU ({})".format(your_choice,CPU))
+        if your_choice == CPU:
+            print(f"You both selected {your_choice}. It's a tie, No winner no Losser!")
+        elif your_choice == "R":
+            if CPU == "S":
+                print("You win!")
+                break
+            else:
+                print("CPU wins.")
+                break
+        elif your_choice == "P":
+            if CPU == "R":
+                print("You win!")
+                break
+            else:
+                print("CPU wins.")
+                break
+        elif your_choice == "S":
+            if CPU == "P":
+                print("You win!")
+                break
+            else:
+                print("CPU wins.")
+                break
+        else:
+            print("invalid input")
+    except:
+        print("something is wrong")
+
